189db37c8f9478201d85f18499cc4b9f14d6c5d6
Matheus Vizzotto dos Santos <86991045+VizzottoMatheus@users.noreply.github.com>
2021-08-23 07:03:02 -0300

    Update confidence_intervals.R

--- Statistical_Inference/confidence_intervals.R
+++ Statistical_Inference/confidence_intervals.R
@@ -1,10 +1,19 @@
 library(tigerstats)
 
-# FUNÇÃO QUE ENCONTRA OS LIMITES INFERIOR E SUPERIOR
+######################################## FUNÇÕES #########################################
+
+# AJUSTA % DE CONFIANÇA
+
+q_adj <- function(ci) {
+  x <- ci + ((1-ci)/2)
+  return(x)
+}
+
+# ENCONTRA LIMITES INFERIOR E SUPERIOR
 
 interval <- function(avg, ci, sd, n) {
-  z2 <- ci + ((1 - ci)/2)     # nível de confiança + alfa/2 (% à esquerda do intervalo) para encontrar o percentil e z correspondente
-  z <- qnorm(z2)
+  q <- q_adj(ci)    
+  z <- qnorm(q)
   error <- z*(sd/sqrt(n))
   sup <- avg + error
   inf <- avg - error
@@ -12,7 +21,7 @@ interval <- function(avg, ci, sd, n) {
   print(paste0("Limite superior: ", round(sup,2)))
 }
 
-##### EXEMPLO #####
+####################################### EXEMPLO ##########################################
 
 # A duração da vida de uma peça de equipamento é tal que o 
 # desvio padrão populacional é igual a 5 horas. Foram amostradas 
@@ -20,15 +29,31 @@ interval <- function(avg, ci, sd, n) {
 # Construir um intervalo de confiança para a verdadeira duração média 
 # da peça com um nível de 95% de confiança.
 
-dp <- 5
+dp <- 5 
 n <- 100
 avg <- 500
 ci <- 0.95
 
-# CURVA NORMAL
 
-pnormGC(bound=qnorm(ci + ((1 - ci)/2), mean = 500, sd =5), region="below", mean = 500, sd = 5, graph=TRUE)
+# PARA ENCONTRAR Z COM UM NÍVEL DE CONFIANÇA DE 95%, DEVE-SE SOMAR O 
+# ALFA (1 - IC) À ESQUERDA DO INTERVALO:
+
+q <- q_adj(ci)
+print(q)  
+
+# O INTERVALO DE CONFINAÇA DE 95% CORRESPONDE
+# AO QUANTIL 97,5% DE UMA DISTRIBUIÇÃO NORMAL
+
+pnormGC(bound=qnorm(ci + ((1 - ci)/2)), region="below", graph = TRUE)
+
+# ENCONTRAMOS Z = 1,96. ESTE É O INTERVALO QUE PROCURAMOS:
+
+pnormGC(bound = c(-qnorm(q), qnorm(q)), region = "between", graph = TRUE)
+
 
 # INTERVALO DE CONFIANÇA
 
 interval(avg, ci, dp, n)
+
+# Limite inferior do intervalo: 499.02
+# Limite superior do intervalo: 500.98
