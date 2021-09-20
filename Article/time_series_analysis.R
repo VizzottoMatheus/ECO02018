06a752dd0b7bd080ae1e7d3964386f397c9d11f0
Matheus Vizzotto dos Santos <matheus.vizzotto515@gmail.com>
2021-09-20 19:59:49 -0300

    decomposition analysis

--- Article/time_series_analysis.R
+++ Article/time_series_analysis.R
@@ -1,4 +1,67 @@
-source("oil_sales_scraping.R")
+source("get_data.R")
+library(forecast)
+library(lubridate)
+library(TSstudio)
+library(ggplot2)
+library(scales)
 
 df_cidades <- oil_data_cities()
 df_rs <- oil_data_state()
+
+head(df_rs)
+tail(df_rs)
+
+### OBJETO TIME SERIES
+
+date_min_y <- year(min(df_rs$ANO_MES))
+date_min_m <- month(min(df_rs$ANO_MES))
+date_min <- c(date_min_y, date_min_m)
+
+date_max_y <- year(max(df_rs$ANO_MES))
+date_max_m <- month(max(df_rs$ANO_MES))
+date_max <- c(date_max_y, date_max_m)
+
+diesel_ts <- ts(df_rs$VENDAS, start = date_min, end = date_max, frequency = 12)
+ts_info(diesel_ts)
+
+### VISUALIZAÇÃO INICIAL
+
+ts_plot(diesel_ts,
+        color = "green",
+        Ygrid = TRUE,
+        Xgrid = TRUE,
+        title = "VENDAS DE ÓLEO DIESEL NO RIO GRANDE DO SUL (METROS CÚBICOS)")
+
+trend <- rollapply(diesel_ts, width = 12, FUN = mean)
+df <- cbind(diesel_ts, trend)
+colnames(df) <- c("VENDAS", "MÉDIA MÓVEL 12 MESES")
+ts_plot(df,
+        color = "green",
+        Ygrid = TRUE,
+        Xgrid = TRUE,
+        title = "VENDAS DE ÓLEO DIESEL NO RIO GRANDE DO SUL (METROS CÚBICOS")
+
+### DECOMPOSIÇÃO
+
+dec <- decompose(diesel_ts)
+plot(dec)
+
+ggseasonplot(diesel_ts) + 
+  theme_bw() +
+  scale_y_continuous(labels = scales::number) +
+  labs(title = "SAZONALIDADE DAS VENDAS DE ÓLEO DIESEL")
+
+ggseasonplot(diff(diesel_ts)) + 
+  theme_bw() +
+  scale_y_continuous(labels = scales::number) +
+  labs(title = "SAZONALIDADE DAS VENDAS DE ÓLEO DIESEL (SÉRIE DIFERENCIADA")
+
+ggsubseriesplot(diesel_ts) +
+  theme_bw() +
+  scale_y_continuous(labels = scales::number) +
+  labs(title = "SAZONALIDADE DAS VENDAS DE ÓLEO DIESEL")
+
+ggsubseriesplot(diff(diesel_ts)) +
+  theme_bw() +
+  scale_y_continuous(labels = scales::number) +
+  labs(title = "SAZONALIDADE DAS VENDAS DE ÓLEO DIESEL (SÉRIE DIFERENCIADA")
